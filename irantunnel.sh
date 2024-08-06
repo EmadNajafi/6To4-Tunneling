eb53d68fbda2cdcebb2eef10de3247da90a8b2b8
Emad Najafi <97056166+EmadNajafi@users.noreply.github.com>
2024-08-06 16:10:21 +0330

    Add files via upload

---
+++ irantunnel.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+print() {
+    text="$1"
+    delay="$2"
+    for ((i=0; i<${#text}; i++)); do
+        echo -n "${text:$i:1}"
+        sleep $delay
+    done
+    echo
+}
+
+clear
+echo ""
+print "Start Tunneling...!" 0.10
+echo ""
+echo ""
+print "Plaese Wait..." 0.10
+echo ""
+echo ""
+
+sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
+po=$(cat /etc/ssh/sshd_config | grep "^Port")
+port=$(echo "$po" | sed "s/Port //g")
+ipiran=$(curl -s ipv4.icanhazip.com)
+
+echo -e "\nIp kharej ro vared konid : "
+read khtmp
+if [[ -n "${khtmp}" ]]; then
+    ipkharej=${khtmp}
+fi
+
+echo -e "\nIp Iran ro vared konid : "
+printf "Ip Pishfarz \e[33m${ipiran}\e[0m, Khali bezarid baraye estefade az ip zir! "
+read irtmp
+if [[ -n "${irtmp}" ]]; then
+    ipiran=${irtmp}
+fi
+
+ip tunnel add 6to4_To_KH mode sit remote $ipkharej local $ipiran
+ip -6 addr add fc00::1/64 dev 6to4_To_KH
+ip link set 6to4_To_KH mtu 1480
+ip link set 6to4_To_KH up
+
+ip -6 tunnel add GRE6Tun_To_KH mode ip6gre remote fc00::2 local fc00::1
+ip addr add 192.168.13.1/30 dev GRE6Tun_To_KH
+ip link set GRE6Tun_To_KH mtu 1436
+ip link set GRE6Tun_To_KH up
+
+sysctl net.ipv4.ip_forward=1
+iptables -t nat -A PREROUTING -p tcp --dport $port -j DNAT --to-destination 192.168.13.1
+iptables -t nat -A PREROUTING -j DNAT --to-destination 192.168.13.2
+
+clear
+echo ""
+print "Done...!" 0.10
+echo ""
+
+
+
