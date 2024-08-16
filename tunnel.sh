912f35136d4ae330ebb93f04f823bafa575ff072
Emad Najafi <97056166+EmadNajafi@users.noreply.github.com>
2024-08-16 14:22:06 +0330

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -78,8 +78,8 @@ port=$(echo "$po" | sed "s/Port //g")
 ipkharej=$(curl -s ipv4.icanhazip.com)
 
 
-echo -e "\nIp Kharej ro vared konid : "
 printf "Ip Server Kharej Shoma : \e[33m${ipkharej}\e[0m, Baraye Taeid Enter Bznid... "
+echo -e "\nIp Kharej ro vared konid : "
 read khtmp
 if [[ -n "${khtmp}" ]]; then
     ipkharej=${khtmp}
