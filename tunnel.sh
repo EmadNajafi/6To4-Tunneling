b49ad130acef029a30290bd7251cd7249c253db3
Emad Najafi <97056166+EmadNajafi@users.noreply.github.com>
2024-08-19 02:16:03 +0330

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+print() {
+    text="$1"
+    delay="$2"
+    for ((i=0; i<${#text}; i++)); do
+        echo -n "${text:$i:1}"
+        sleep $delay
+    done
+    echo
+}
 
 ##tunnelmenu##
 tunnelmenu () { while true
